19cbc8c26d0f5f23d150f388abd301a13a50df75
张瑞卿 <1315993841@qq.com>
2025-08-19 23:16:41 +0800

    添加 inspira-ui BentoGrid组件

---
+++ src/components/inspira-ui/BentoGrid/BentoGrid.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { cn } from '@/lib/utils'
+import type { HTMLAttributes } from 'vue'
+
+interface Props {
+  class?: HTMLAttributes['class']
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        'grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
